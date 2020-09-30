a3563b4c84ccb244b450677d1d3cc7c2dd4e05aa
Loïc Bellemare-Alford <loicbellemare@hotmail.com>
2020-09-30 16:06:56 -0400

    Added components type

--- @types/components.d.ts
+++ @types/components.d.ts
@@ -1,4 +1,5 @@
 import React from "react"
+import { Components } from '../src/index';
 
 export type BlockDefinition = {
     id: String,
@@ -29,3 +30,5 @@ export type BlockProps = {
 }
 
 export type BlockType = (props: BlockProps, context?: any) => React.ReactNode
+
+export type Components = Components
