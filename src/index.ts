88485b17bb58a2d467c232e46a3c1d09443edd3d
Loïc Bellemare-Alford <loicbellemare@hotmail.com>
2020-09-30 15:54:48 -0400

    Components class

--- src/index.ts
+++ src/index.ts
@@ -1,64 +1,65 @@
-import * as BasicComponents from "./basic-components"
-import { BlockDefinition } from "../@types/components"
+import * as BasicComponents from "./basic-components";
+import { BlockDefinition } from "../@types/components";
 
-let componentsList: BlockDefinition[] = []
-let componentsMap: Map<String, BlockDefinition> = new Map()
+export class Components {
+  componentsList: BlockDefinition[] = [];
+  componentsMap: Map<String, BlockDefinition> = new Map();
 
-export const addComponents = (newComponents: BlockDefinition[]) => {
-    componentsList = [
-        ...componentsList,
-        ...newComponents
-    ]
-    generateMap(componentsList)
-}
+  constructor(components =[]) {
+    this.addComponents(BasicComponents.components);
+    if (components) this.addComponents(components)
+  }
 
-export const generateMap = (list: BlockDefinition[]) => {
+  addComponents = (newComponents: BlockDefinition[]) => {
+    this.componentsList = [...this.componentsList, ...newComponents];
+    this.generateMap(this.componentsList);
+  };
+
+  generateMap = (list: BlockDefinition[]) => {
     list.map((component) => {
-        componentsMap.set(component.id, component)
-    })
-}
+      this.componentsMap.set(component.id, component);
+    });
+  };
 
-export const getComponentsDefinitions = () => {
-    return componentsList.map((component) => ({
-        id: component.id,
-        ...component.definition
-    }))
-}
+  getComponentsDefinitions = () => {
+    return this.componentsList.map((component) => ({
+      id: component.id,
+      ...component.definition,
+    }));
+  };
 
-export const getDefinition = (id: String) => {
-    const component = componentsMap.get(id)
-    return component ? {
-        id: id,
-        ...component.definition,
-        // customProps: component.customProps
-    } : {}
-}
+  getDefinition = (id: String) => {
+    const component = this.componentsMap.get(id);
+    return component
+      ? {
+          id: id,
+          ...component.definition,
+          // customProps: component.customProps
+        }
+      : {};
+  };
 
-export const getReactComponent: React.ReactNode = (id: String, isCanvas: Boolean) => {
-    const component = componentsMap.get(id)
-    if (!component) return null
-    return isCanvas ?
-        component.canvas :
-        component.component
-}
+  getReactComponent: React.ReactNode = (id: String, isCanvas: Boolean) => {
+    const component = this.componentsMap.get(id);
+    if (!component) return null;
+    return isCanvas ? component.canvas : component.component;
+  };
 
-export const getDefaultChildren: React.ReactNode = (id: String) => {
-    const component = componentsMap.get(id)
-    return component ? component.children : null
-}
+  getDefaultChildren: React.ReactNode = (id: String) => {
+    const component = this.componentsMap.get(id);
+    return component ? component.children : null;
+  };
 
-export const getDefaultProps = (id: String) => {
-    let props: any = {}
-    const component = componentsMap.get(id)
-    if (!component) return null
-    component.props.map((prop: any) => props[prop.name] = prop.default)
-    return props
-}
+  getDefaultProps = (id: String) => {
+    let props: any = {};
+    const component = this.componentsMap.get(id);
+    if (!component) return null;
+    component.props.map((prop: any) => (props[prop.name] = prop.default));
+    return props;
+  };
 
-export const getProps = (id: String) => {
-    const component = componentsMap.get(id)
-    return component ? component.props : {}
+  getProps = (id: String) => {
+    const component = this.componentsMap.get(id);
+    return component ? component.props : {};
+  };
 }
-
-/* Initialize */
-addComponents(BasicComponents.components)
