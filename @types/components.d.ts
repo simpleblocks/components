bd37e3c2ac983024179f89f9aefc3c0276ae799f
Loïc Bellemare-Alford <loicbellemare@hotmail.com>
2021-04-26 17:30:50 -0400

    Added class declaration

--- @types/components.d.ts
+++ @types/components.d.ts
@@ -46,3 +46,18 @@ export interface ComponentsInstance {
   getDefaultProps: (id: String) => Object | null,
   getProps: (id: String) => Object
 }
+
+export declare class Components implements ComponentsInstance {
+  componentsList: BlockDefinition[];
+  componentsMap: Map<String, BlockDefinition>;
+  constructor(components?: BlockDefinition[]);
+  addComponents: (newComponents: BlockDefinition[]) => void;
+  generateMap: (list: BlockDefinition[]) => void;
+  getComponentsDefinitions: () => BlockDefDetails[];
+  getDefinition: (id: String) => {};
+  getReactComponent: (id: String, isCanvas: Boolean) => {} | null | undefined;
+  getDefaultChildren: (id: String) => {} | null | undefined;
+  getDefaultProps: (id: String) => any;
+  getProps: (id: String) => {};
+}
+export default Components;
